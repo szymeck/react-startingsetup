0a22f8d3b1bcd68ff451e86845fc6be5f6f2a9bc
szymeck <szymek.sobierajski@gmail.com>
2023-01-18 17:06:12 +0100

    reactwork2

---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,17 @@
+import "./ExpenseDate.css";
+import Card from "../UI/Card";
+
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString("en-Us", { month: "long" });
+  const day = props.date.toLocaleString("en-Us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <Card className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </Card>
+  );
+};
+
+export default ExpenseDate;
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  // const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enterdAmout: "",
+  //     enteredDate: "",
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const [enteredAmount, setEnteredAmout] = useState("");
+  const amoutChangeHandler = (event) => {
+    setEnteredAmout(event.target.value);
+  };
+  const [enteredDate, setEnteredDate] = useState("");
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amout: enteredAmount,
+      date: new Date(enteredDate)
+    };
+    console.log(expenseData);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amout</label>
+          <input
+            type="number"
+            onChange={amoutChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            onChange={dateChangeHandler}
+            min="2019-01-01"
+            max="2023-12-31"
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
